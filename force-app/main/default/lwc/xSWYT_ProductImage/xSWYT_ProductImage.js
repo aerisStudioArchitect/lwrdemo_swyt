6999169a70ffe867f5df3c08595f9b78c7af37db
A Forrest <aeris.forrest@studioscience.com>
2025-02-10 10:25:08 -0600

    added placeholder functionality for adding items to a wishlist with custom apex controller class (class not included)

--- force-app/main/default/lwc/xSWYT_ProductImage/xSWYT_ProductImage.js
+++ force-app/main/default/lwc/xSWYT_ProductImage/xSWYT_ProductImage.js
@@ -1,4 +1,11 @@
 import { LightningElement, api, track } from 'lwc';
+import isGuestUser from "@salesforce/user/isGuest";
+import userId from '@salesforce/user/Id';
+import COMMUNITY_ID from '@salesforce/community/Id';
+import sitePath from "@salesforce/community/basePath";
+
+/** UNCOMMENT THE FOLLOWING LINE TO ENABLE APEX CONTROLLER FOR WISHLIST FUNCTIONALITY */
+//import addProductToWishlist from '@salesforce/apex/WishlistButtonController';
 
 export default class XSWYT_ProductImage extends LightningElement {
     @api imageUrl;
@@ -74,5 +81,43 @@ export default class XSWYT_ProductImage extends LightningElement {
         console.log('removing product...');
         //add functionality to remove product from wishlist
         this.productInWishlist = false;
+    }
+
+
+    
+    /**
+     * Update component to call this function when used with an environment where Commerce is enabled
+     * 
+     */
+    async addProductToWishlist(){
+        //console.log('await addToWishlist('+this.recordId+','+COMMUNITY_ID+','+this.accountId+','+this.userId+');');
+        //console.log('calling apex method');
+
+        //call Apex method to add product to wishlist using the ConnectApi
+        await addToWishlist({prodId: this.recordId, communityId: COMMUNITY_ID,acctId: this.accountId,userId: this.userId})
+            .then((result) => {
+                // handle result
+                /* console.log('added to wishlist');
+                console.log(result);*/
+                if(result){
+                    var ProductURLVar = '/'+sitePath+'/mylists';
+                    var message = 'Product successfully added to wishlist. ';
+
+                    /** TODO: ADD/UPDATE CUSTOM COMPONENT TO SHOW A CONFIRMATION MESSAGE WHEN THE ITEM IS SUCCESSFULLY ADDED TO THE WISHLIST */
+                    /** EX: this.template.querySelector('c-custom-toast-cmp').showToast('success',message,ProductURLVar,'View Wishlist'); */
+                }
+            })
+            .catch ((error) => {
+                // handle errors
+                console.log(error);
+                this.error = error;const err = error?.error ?? error;
+                //DO NOT ALLOW GUESTS TO ADD ITEMS TO A WISHLIST BECAUSE THEY DO NOT HAVE A WISHLIST
+                if (err?.code === 'GUEST_INSUFFICIENT_ACCESS') {
+                    navigate(this.navContext, loginPage);
+                } else{
+                    /** TODO: HANDLE ERROR TO SHOW RELEVANT MESSAGING WHEN THE ITEMS IS NOT SUCCESSFULLY ADDED TO THE WISHLIST */
+                    /** EX: this.template.querySelector('c-x-studio-_-custom-toast-cmp').showToast('Error', error.message,null,null); */
+                }
+            });
     }
 }
